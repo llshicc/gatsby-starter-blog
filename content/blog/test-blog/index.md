36e5a419509536ad1002635eae8247e386e7cb32
Llshi <sxwc017560@outlook.com>
2020-04-05 19:55:50 +0800

    up

---
+++ content/blog/test-blog/index.md
@@ -0,0 +1,7 @@
+---
+title: Test
+date: "2020-04-05T15:40:32.169Z"
+description: This is a test blog
+---
+
+Test contents
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `LLshi's Blog`,
     author: {
       name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      summary: `who try to be developer and designer.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
